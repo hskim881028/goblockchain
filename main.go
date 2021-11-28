a74542acdd053a06fd7b49e1a8c12869af250de9
hskim <andykim881028@gmail.com>
2021-11-28 22:01:50 +0900

    Add explorer.go file

--- main.go
+++ main.go
@@ -1,49 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"text/template"
-
-	"github.com/hskim881028/goblockchain/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"home", blockchain.GetBlcokchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlcokchain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
+import explorer "github.com/hskim881028/goblockchain/explorer/templates"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
