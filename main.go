294e1f0e37f112c4fc8c20e073349027cb35ec5d
hskim <andykim881028@gmail.com>
2021-11-30 01:03:57 +0900

    Encode data to json

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import explorer "github.com/hskim881028/goblockchain/explorer/templates"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         "/blocks",
+			Method:      "POST",
+			Description: "Add a block",
+			Payload:     "data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	// b, err := json.Marshal(data)
+	// utility.HandleErr(err)
+	// fmt.Printf("%s", b)
+	json.NewEncoder(rw).Encode(data)
+
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on Http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
