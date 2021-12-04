c2d56beb59988350838e48691907589508bdf71d
hskim <andykim881028@gmail.com>
2021-12-05 03:04:14 +0900

    Add json Content-Type middleware func

--- rest/rest.go
+++ rest/rest.go
@@ -60,19 +60,19 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See a block",
 		},
 	}
-
-	rw.Header().Add("Content-Type", "application/json")
-	// b, err := json.Marshal(data)
-	// utility.HandleErr(err)
-	// fmt.Printf("%s", b)
 	json.NewEncoder(rw).Encode(data)
+}
 
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
 }
 
 func blocks(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(rw).Encode(blockchain.GetBlcokchain().AllBlocks())
 	case "POST":
 		var addBlockBody addBlockBody
@@ -98,6 +98,7 @@ func block(rw http.ResponseWriter, r *http.Request) {
 func Start(aPort int) {
 	port = fmt.Sprintf(":%d", aPort)
 	router := mux.NewRouter()
+	router.Use(jsonContentTypeMiddleware)
 	router.HandleFunc("/", documentation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET")
