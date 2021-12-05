02a5c88d8bfcb15c896743321c25a1ebc506c572
hskim <andykim881028@gmail.com>
2021-12-05 23:05:28 +0900

    Add db.file

---
+++ db/db.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/boltdb/bolt"
+	"github.com/hskim881028/goblockchain/utility"
+)
+
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open(dbName, 0060, nil)
+		db = dbPointer
+		utility.HandleErr(err)
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utility.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utility.HandleErr(err)
+	}
+	return db
+}
+
+func SaveBlock(hash string, data []byte) {
+	fmt.Printf("Save block\n hash : %s\n data : %b", hash, data)
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blocksBucket))
+		err := bucket.Put([]byte(hash), data)
+		return err
+	})
+	utility.HandleErr(err)
+}
+
+func SaveBlockChain(data []byte) {
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
+		err := bucket.Put([]byte("checkPoint"), data)
+		return err
+	})
+	utility.HandleErr(err)
+}
