af0a5e77b57aed483f5c03b462b283f4424c0a4f
hskim <andykim881028@gmail.com>
2021-12-10 01:25:01 +0900

    Add mine func

--- blockchain/block.go
+++ blockchain/block.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/hskim881028/goblockchain/db"
 	"github.com/hskim881028/goblockchain/utility"
@@ -14,11 +15,13 @@ type Block struct {
 	Hash       string `json:hash`
 	PrevHash   string `json:prevHash,omitempty`
 	Height     int    `json:height`
-	Defficulty int    `json:difficulty`
+	Difficulty int    `json:difficulty`
 	Nonce      int    `json:nonce`
 }
 
 var ErrorNotFound = errors.New("block not found")
+
+const difficulty int = 2
 
 func (b *Block) Persist() {
 	db.SaveBlock(b.Hash, utility.ToBytes(b))
@@ -28,15 +31,31 @@ func (b *Block) restore(data []byte) {
 	utility.FromBytes(b, data)
 }
 
+func (b *Block) mine() {
+	target := strings.Repeat("0", difficulty)
+	for {
+		blockAsString := fmt.Sprint(b)
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
+		fmt.Printf("Block as String:%s\nHash:%s\nTarget:%s\nNonce:%d\n\n\n", blockAsString, hash, target, b.Nonce)
+		if strings.HasPrefix(hash, target) {
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
 func createBlock(data string, prevHash string, height int) *Block {
 	block := &Block{
-		Data:     data,
-		Hash:     "",
-		PrevHash: prevHash,
-		Height:   height,
+		Data:       data,
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficulty: difficulty,
+		Nonce:      0,
 	}
-	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	block.mine()
 	block.Persist()
 	return block
 }
--- main.go
+++ main.go
@@ -1,28 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/hskim881028/goblockchain/blockchain"
+	"github.com/hskim881028/goblockchain/cli"
 )
 
 func main() {
-	proofOfWork()
-	// blockchain.Blcokchain()
-	// cli.Start()
-}
-
-func proofOfWork() {
-	difficulty := 2
-	nonce := 1
-	target := strings.Repeat("0", difficulty)
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("Test"+fmt.Sprint(nonce))))
-		fmt.Printf("Target : %s\n Hash : %s\n Nonce : %d\n--------\n", target, hash, nonce)
-		if strings.HasPrefix(hash, target) {
-			break
-		} else {
-			nonce++
-		}
-	}
+	blockchain.Blcokchain()
+	cli.Start()
 }
