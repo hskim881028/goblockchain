6769a9a26e00b86ebd971183c01fa4ec16779dbb
hskim <andykim881028@gmail.com>
2021-12-10 01:05:55 +0900

    pow

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"github.com/hskim881028/goblockchain/blockchain"
-	"github.com/hskim881028/goblockchain/cli"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	blockchain.Blcokchain()
-	cli.Start()
+	proofOfWork()
+	// blockchain.Blcokchain()
+	// cli.Start()
+}
+
+func proofOfWork() {
+	difficulty := 2
+	nonce := 1
+	target := strings.Repeat("0", difficulty)
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("Test"+fmt.Sprint(nonce))))
+		fmt.Printf("Target : %s\n Hash : %s\n Nonce : %d\n--------\n", target, hash, nonce)
+		if strings.HasPrefix(hash, target) {
+			break
+		} else {
+			nonce++
+		}
+	}
 }
