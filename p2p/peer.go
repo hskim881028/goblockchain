4f36bd39e13e3e5d355a2fa58bc1e9786feb5eec
hskim <andykim881028@gmail.com>
2021-12-18 18:58:53 +0900

    Add close func in peer

--- p2p/peer.go
+++ p2p/peer.go
@@ -7,15 +7,24 @@ import (
 )
 
 type peer struct {
-	conn  *websocket.Conn
-	inbox chan []byte
+	conn    *websocket.Conn
+	inbox   chan []byte
+	key     string
+	address string
+	port    string
 }
 
 var Peers map[string]*peer = make(map[string]*peer)
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
 	key := fmt.Sprintf("%s:%s", address, port)
-	p := &peer{conn, make(chan []byte)}
+	p := &peer{
+		conn:    conn,
+		inbox:   make(chan []byte),
+		key:     key,
+		address: address,
+		port:    port,
+	}
 	go p.read()
 	go p.write()
 	Peers[key] = p
@@ -23,6 +32,7 @@ func initPeer(conn *websocket.Conn, address, port string) *peer {
 }
 
 func (p *peer) read() {
+	defer p.close()
 	for {
 		_, m, err := p.conn.ReadMessage()
 		if err != nil {
@@ -33,8 +43,17 @@ func (p *peer) read() {
 }
 
 func (p *peer) write() {
+	defer p.close()
 	for {
-		m := <-p.inbox
+		m, ok := <-p.inbox
+		if !ok {
+			break
+		}
 		p.conn.WriteMessage(websocket.TextMessage, m)
 	}
+}
+
+func (p *peer) close() {
+	p.conn.Close()
+	delete(Peers, p.key)
 }
